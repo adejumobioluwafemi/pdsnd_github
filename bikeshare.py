44472699f72a1e85d5a454d57098ff2127985ccf
Adejumobi Oluwafemi <Adejumobi.Oluwafemi@fintraksoftware.com>
2022-01-14 14:41:27 +0100

    refactor: Added a comment line in the code

--- bikeshare.py
+++ bikeshare.py
@@ -207,6 +207,7 @@ def main():
         trip_duration_stats(df)
         user_stats(df,city)
 
+        # to handle user experience
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
